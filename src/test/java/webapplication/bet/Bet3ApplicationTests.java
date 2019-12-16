8058653b7449142dd39119e59cac46507c759f33
Raval97 <gegotek.rafal@gmail.com>
2019-12-16 17:03:08 +0100

    START

---
+++ src/test/java/webapplication/bet/Bet3ApplicationTests.java
@@ -0,0 +1,13 @@
+package webapplication.bet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bet3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
